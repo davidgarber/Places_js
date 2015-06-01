8ae1c05659e3ab922d9149749d818864aa3dd3ac
Jocelyn Alsdorf & David Garber <student@epicodus.com>
2015-06-01 15:33:33 -0700

    refactor so that it mets assignment criteria

--- js/scripts.js
+++ js/scripts.js
@@ -3,39 +3,49 @@ $(document).ready(function() {
     $("#new-location").append('<div class="new-location">' +
     '<div class="form-group">' + '<label for="new-city">City</label>' +
     '<input type="text" class="form-control new-city">' + '</div>' +
-     '<div class="form-group">' + '<label for="new-country">Country</label>' +
-     '<input type="text" class="form-control new-country">' + '</div>' +
-     '</div>');
+     '<div class="form-group">' + '<label for="new-landmark">Landmark</label>' +
+     '<input type="text" class="form-control new-landmark">' + '</div>' +
+      '<div class="form-group">' + '<label for="new-timeOfYear">Time of Year</label>' +
+      '<input type="text" class="form-control new-timeOfYear">' + '</div>' +
+       '<div class="form-group">' + '<label for="new-notes">Notes</label>' +
+       '<input type="text" class="form-control new-notes">' + '</div>' +
+       '</div>' );
   });
 
   $("form#new-location").submit(function(event){
+
+
     event.preventDefault();
-    var inputtedName = $("input#new-first-name").val();
-    var newPerson = {firstName: inputtedName, locations: []};
+    var inputtedCountry = $("input#new-country").val();
+    var newPlace = {country: inputtedCountry, locations: []};
 
   $(".new-location").each(function(){
       var inputtedCity = $(this).find("input.new-city").val();
-      var inputtedCountry = $(this).find("input.new-country").val();
+      var inputtedLandmark = $(this).find("input.new-landmark").val();
+      var inputtedTimeOfYear = $(this).find("input.new-timeOfYear").val();
+      var inputtedNotes = $(this).find("input.new-notes").val();
 
-      var newLocation = {city: inputtedCity, country: inputtedCountry};
-      newPerson.locations.push(newLocation);
+
+      var newLocation = {city: inputtedCity, landmark: inputtedLandmark, timeOfYear: inputtedTimeOfYear, notes: inputtedNotes};
+
+      newPlace.locations.push(newLocation);
     });
-  $("ul#person").append("<li><span class='person-li'>" + newPerson.firstName +"</span></li>");
 
-  $(".person-li").last().click(function() {
+  $("ul#country").append("<li><span class='country-li'>" + newPlace.country +"</span></li>");
+
+  $(".country-li").last().click(function() {
     $(".hidden-list").show();
-    $(".name").text(newPerson.firstName);
+    $(".name").text(newPlace.country);
     $("ul#kewl-places").text("");
-      newPerson.locations.forEach(function(location) {
-        $("ul#kewl-places").append("<li>" + location.city + "," + location.country + "</li>");
+      newPlace.locations.forEach(function(location) {
+        $("ul#kewl-places").append("<li>" + location.city + "," + location.landmark + "," + location.timeOfYear + "," + location.notes + "</li>");
       });
   });
 
-
-    $("input#new-first-name").val("");
+    $("input#new-country").val("");
     $("input.new-city").val("");
-    $("input.new-country").val("");
-
-
+    $("input.new-landmark").val("");
+    $("input.new-timeOfYear").val("");
+    $("input.new-notes").val("");
   });
 });
